e484097bee781854a01485e4c036258358daa957
Susana Uribe Velásquez <susyuribe22@gmail.com>
2024-11-18 14:12:37 -0500

    fix: remove hardcoded port in Vite server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
     server: {
-        host: '0.0.0.0',  // Asegúrate de que Vite escuche en todas las interfaces de red
-        port: 5173,       // El puerto que Render necesita escuchar, puedes cambiarlo si es necesario
+        host: '0.0.0.0',  
       },
     plugins: [react()],
     resolve: {
