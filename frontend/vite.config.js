11909d9b367362c588c95eb481ebacc36ef908f8
Susana Uribe Velásquez <susyuribe22@gmail.com>
2024-11-18 14:00:03 -0500

    fix: update Vite server port to 5173 for compatibility with Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     server: {
         host: '0.0.0.0',  // Asegúrate de que Vite escuche en todas las interfaces de red
-        port: 3000,       // El puerto que Render necesita escuchar, puedes cambiarlo si es necesario
+        port: 5173,       // El puerto que Render necesita escuchar, puedes cambiarlo si es necesario
       },
     plugins: [react()],
     resolve: {
