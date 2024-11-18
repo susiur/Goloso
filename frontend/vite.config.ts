3a9e6be61bf8ac76f72fcb2d238a94dc735644a8
Susana Uribe Velásquez <susyuribe22@gmail.com>
2024-11-18 14:48:57 -0500

    feat: configure Vite server to use environment variable for port and listen on all interfaces

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+},
   plugins: [react()],
   resolve: {
     alias: {
