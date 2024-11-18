4059ec1a117bc99fb3a9d511141d71cc97a54cc7
Susana Uribe Velásquez <susyuribe22@gmail.com>
2024-11-18 15:03:43 -0500

    fix: update CORS origin to allow requests from Render deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://goloso-0b2a.onrender.com/',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // Si usas cookies o autenticación, habilita esto
