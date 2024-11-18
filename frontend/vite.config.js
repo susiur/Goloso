a792e59aa0432a3d596ea1f25db7948d18e40c05
Susana Uribe Velásquez <susyuribe22@gmail.com>
2024-11-18 13:49:40 -0500

    feat: configure Vite server to listen on all network interfaces and set port to 3000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,10 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',  // Asegúrate de que Vite escuche en todas las interfaces de red
+        port: 3000,       // El puerto que Render necesita escuchar, puedes cambiarlo si es necesario
+      },
     plugins: [react()],
     resolve: {
         alias: {
