6113f793bfe5e54542d2fa47d2ed55bd245f6f9b
Susana Uribe Velásquez <susyuribe22@gmail.com>
2024-11-18 14:27:38 -0500

    fix: update Vite server configuration to use environment variable for port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
     server: {
-        host: '0.0.0.0',  
-      },
+        host: '0.0.0.0',
+        port: process.env.PORT || 4173,
+    },
     plugins: [react()],
     resolve: {
         alias: {
