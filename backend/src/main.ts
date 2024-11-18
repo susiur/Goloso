43412717623f87005ca848cdd2a4cff5ec593504
Susana Uribe Velásquez <susyuribe22@gmail.com>
2024-11-18 15:07:55 -0500

    fix: update CORS configuration to allow requests from any origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://goloso-0b2a.onrender.com/',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // Si usas cookies o autenticación, habilita esto
